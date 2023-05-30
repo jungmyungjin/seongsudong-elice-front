1ad06fce12634e861cd8a999198b7067b934f534
shinhayeong <judy.hayoung.shin@gmail.com>
2023-05-30 13:12:05 +0900

    feat : 플로팅 버튼 레이아웃 완성

--- src/components/common/FloatingButton/index.tsx
+++ src/components/common/FloatingButton/index.tsx
@@ -5,12 +5,9 @@ function FloatingButton() {
   return (
     <div className={styles.floatingButtonContainer}>
       <button className={styles.floatingButton}>
+        <div className={styles.floatingButtonText}>문의하기</div>
         <div className={styles.rabbitIcon}>
-          <img
-            src='images/rabbit.png'
-            alt='rabbit-icon'
-            className={styles.rabbitImage}
-          />
+          <img src='images/rabbit.png' alt='rabbit-icon' />
         </div>
       </button>
     </div>
