624cde86dfa8c372e2a070680dba02085fc550fe
jungmyungjin <myung2667@gmail.com>
2023-05-31 00:58:14 +0900

    feat : Header 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/index.scss';
 
 import Main from './pages/Main';
+import Header from './components/common/Header';
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Main />} />
         </Routes>
