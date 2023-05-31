2e967129536c9e7c0c1dbe483ac8741cd6daf095
[SW4/2반] 성치호 <clghdi9898@gmail.com>
2023-06-01 00:11:03 +0900

    feat: 상세 게시물 초기 디자인 제작

--- src/components/Board/PostsDetail.tsx
+++ src/components/Board/PostsDetail.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';  // useParams는 현재 URL의 파라미터를 가져올 수 있는 Hook입니다.
 import axios from 'axios';
+import styles from './postsDetail.module.scss';
+import FloatingButton from 'components/common/FloatingButton';
 
 interface Post {
   id: number;
@@ -29,8 +31,24 @@ const PostDetail: React.FC = () => {
   // post가 null이 아니면 게시물 정보를 보여줍니다.
   return (
     <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
+      <div className={styles.postDetail}>
+        <div className={styles.title}>
+          <p>{post.title}</p>
+        </div>
+        <div className={styles.postInfo}>
+          <p>홍길동 | 2023-05-25 | 조회수 : 30</p>
+        </div>
+        <div className={styles.description}>
+          <p>{post.body}</p>
+        </div>
+        <div className={styles.updateAndDeleteBtn}>
+          <button className={styles.updateBtn}>수정</button>
+          <button className={styles.deleteBtn}>삭제</button>
+        </div>
+        <div className={styles.underLine}>
+          <p>댓글 0개</p>
+        </div>
+      </div>
     </div>
   );
 };
