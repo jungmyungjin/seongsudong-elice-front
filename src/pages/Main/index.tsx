0dac3ac2cd4909b82521b81069d19d26422053d4
jungmyungjin <myung2667@gmail.com>
2023-05-31 17:04:19 +0900

    feat: 메인 페이지 레이아웃 및 버튼 링크 구현

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,21 +1,30 @@
-import React, { useEffect } from 'react';
-import ConfirmModal from 'components/common/ConfirmModal';
-import { useAppDispatch, useAppSelector } from 'hooks/useRedux';
-import { openConfirmModal } from 'reducers/modal';
+import React from 'react';
+import styles from './main.module.scss';
+import { useNavigate } from 'react-router-dom';
 
-export default function Main() {
-  const { isConfirmModalOpen } = useAppSelector(state => state.modal);
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(openConfirmModal());
-  }, []);
+const Main = (): React.ReactElement => {
+  let navigate = useNavigate();
+
+  const handleClick = (path: string) => {
+    navigate(path);
+  };
   return (
-    <>
-      <div>
-        {isConfirmModalOpen && (
-          <ConfirmModal modalMessage='게시물을 삭제하시겠습니까?' />
-        )}
+    <div className={styles.MainLayout}>
+      <div className={styles.imageSlider}>이미지 슬라이더</div>
+      <div className={styles.noticeBoard}>
+        <button onClick={() => handleClick('/post')}>공지사항</button>
       </div>
-    </>
+      <button
+        onClick={() => handleClick('/reservation')}
+        className={styles.reservation}
+      >
+        예약하기
+      </button>
+      <div className={styles.freeBoard}>
+        <button onClick={() => handleClick('/post')}>자유 게시판</button>
+      </div>
+    </div>
   );
-}
+};
+
+export default Main;
