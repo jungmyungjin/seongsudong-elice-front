15e243be35ff0818a756661d3b71ebbde03c479a
[SW4/2반] 성치호 <clghdi9898@gmail.com>
2023-05-31 17:41:58 +0900

    feat: 게시물 생성 페이지 구현

--- src/components/Board/CreatePost.tsx
+++ src/components/Board/CreatePost.tsx
@@ -1,26 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import styles from './createPost.module.scss';
+import { ReactComponent as Back } from 'assets/Back.svg';
+import { ReactComponent as UploadIcon } from 'assets/Upload.svg';
 
 interface FormInput {
   title: string;
   body: string;
+  file: FileList;
 }
 
 const CreatePost: React.FC = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormInput>();
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<FormInput>();
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
+  const fileInputRef = useRef<HTMLInputElement>(null); 
+
+  const onFileButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const file = watch('file'); // file 필드의 변화를 감시합니다.
 
   const onSubmit = async (data: FormInput) => {
     setIsLoading(true);
     try {
-      const response = await axios.post('https://jsonplaceholder.typicode.com/posts', data);
+      const formData = new FormData();
+      formData.append('title', data.title);
+      formData.append('body', data.body);
+      if (file?.length > 0) {
+        formData.append('file', file[0]);
+      }
+
+      const response = await axios.post('https://jsonplaceholder.typicode.com/posts', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+
       if (response.status === 201) {
-        // 포스트가 성공적으로 생성되면, 게시글 목록 페이지로 이동합니다.
-        navigate('/');
+        navigate(-1);
       }
     } catch (error) {
       console.error(error);
@@ -29,23 +50,42 @@ const CreatePost: React.FC = () => {
     }
   };
 
+
   return (
     <div className={styles['create-post-container']}>
-      <h1>새 게시글 생성</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className={styles['form-group']}>
-            <label htmlFor="title">제목</label>
-            <input id="title" {...register('title', { required: true })} />
-            {errors.title && <span>제목은 필수 입력 항목입니다.</span>}
+        <div className={styles.buttonContainer}>
+          {/* 뒤로 가기 버튼 */}
+          <button type="button" onClick={() => navigate(-1)}>
+            <Back />
+          </button>
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? '게시글 생성 중...' : '완료'}
+          </button>
         </div>
         <div className={styles['form-group']}>
-            <label htmlFor="body">내용</label>
-            <textarea id="body" {...register('body', { required: true })} />
-            {errors.body && <span>내용은 필수 입력 항목입니다.</span>}
+          <input 
+            id="title" 
+            className={`${styles.titleInput} ${errors.title ? styles.errorInput : ''}`}
+            placeholder="제목을 입력하세요." 
+            {...register('title', { required: true })}
+          />
+          {errors.title && <span className={styles.errorMessage}>제목은 필수 입력 항목입니다.</span>}
         </div>
-        <button type="submit" disabled={isLoading}>
-          {isLoading ? '게시글 생성 중...' : '게시글 생성'}
-        </button>
+        <div className={styles['form-group']}>
+          <input 
+            id="body" 
+            className={`${styles.bodyInput} ${errors.body ? styles.errorInput : ''}`}
+            placeholder="내용을 입력하세요." 
+            {...register('body', { required: true })}
+          />
+          {errors.body && <span className={styles.errorMessage}>내용은 필수 입력 항목입니다.</span>}
+        </div>
+        {/* 파일 업로드 */}
+        <div className={styles.fileUpload}>
+          <UploadIcon onClick={onFileButtonClick} />
+          <input id="file" type="file" ref={fileInputRef} onChange={e => register('file').onChange(e)} style={{ display: 'none' }} />
+        </div>
       </form>
     </div>
   );
