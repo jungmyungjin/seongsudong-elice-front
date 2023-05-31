28a5a483f8035a37f8fbdc7cd3087123ca671200
조가인 <chogain@jogain-ui-MacBookAir.local>
2023-05-31 18:57:53 +0900

    예약페이지 css 통일, 좌석 종류 클릭 시 해당하는 종류의 좌석 색 변화기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/index.scss';
 import Board from './components/Board/Board';
 import PostsDetail from './components/Board/PostsDetail';
 import CreatePost from './components/Board/CreatePost';
+import Reservation from 'components/Reservation';
 import Main from './pages/Main';
 import FloatingButton from 'components/common/FloatingButton';
 import Admin from 'pages/Admin/Admin';
@@ -13,13 +14,14 @@ function App() {
     <div className='App'>
       <BrowserRouter>
         <Header />
-          <Routes>
-            <Route path='/' element={<Main />} />
-            <Route path='/admin' element={<Admin />} />
-            <Route path="/post"  element={<Board/>} />
-            <Route path="/post/:id" element={<PostsDetail />} />
-            <Route path="/post/create" element={<CreatePost />} />
-          </Routes>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/admin' element={<Admin />} />
+          <Route path='/post' element={<Board />} />
+          <Route path='/post/:id' element={<PostsDetail />} />
+          <Route path='/post/create' element={<CreatePost />} />
+          <Route path='/reservation' element={<Reservation />} />
+        </Routes>
       </BrowserRouter>
       <FloatingButton />
     </div>
