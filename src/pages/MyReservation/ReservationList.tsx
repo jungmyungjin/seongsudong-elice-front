0455d4a57d610ee7cc8afe87a329c5113f72bf26
shinhayeong <judy.hayoung.shin@gmail.com>
2023-06-01 22:28:59 +0900

    chore : 임시 저장

--- src/pages/MyReservation/ReservationList.tsx
+++ src/pages/MyReservation/ReservationList.tsx
@@ -1,64 +1,37 @@
 import styles from './reservationList.module.scss';
-import { MyReservation } from 'types/myReservation';
+
 import { ReactComponent as ChevronRight } from '../../assets/ChevronRight.svg';
 
-import { useAppDispatch, useAppSelector } from 'hooks/useRedux';
-import { openMyReservationModal } from 'reducers/modal';
-
-import MyReservationModal from '../../components/MyReservationModal';
-
-/**
- * 더미 액션 작업 => 추후에 지워야함
- */
-import { createAction } from '@reduxjs/toolkit';
+import { MyReservation } from 'types/myReservation';
 
 interface Props {
-  reservationList: MyReservation[];
+  reservation: MyReservation;
+  onClick: () => void;
 }
 
-function ReservationList({ reservationList }: Props) {
-  const { isMyRevervationModalOpen } = useAppSelector(state => state.modal);
-  const dispatch = useAppDispatch();
-
-  const setMyReservationDetail = createAction<MyReservation>(
-    'myReservation/setMyReservationDetail',
-  );
-
+function ReservationList({ reservation, onClick }: Props) {
   const convertStringToDate = (date: string) => {
     const value = date.split('T')[0];
     return value;
   };
 
-  /* 클릭한 곳의 고유 id로 해당 reservation 데이터를 찾아서 
-  setMyReservationDetail을 통해 dispatch */
-  const handleOpenReservationModal = (id: number) => {
-    const reservation = reservationList.find(item => item.id === id);
-    if (reservation) {
-      dispatch(setMyReservationDetail(reservation));
-    }
-    dispatch(openMyReservationModal());
-  };
-
   return (
     <div className={styles.reservationList}>
-      {isMyRevervationModalOpen && <MyReservationModal />}
-      {reservationList.map((item, _) => (
-        <div
-          className={styles.reservationContainer}
-          key={item.id}
-          onClick={() => handleOpenReservationModal(item.id)}
-        >
-          <div className={styles.eachReservation}>
-            <p className={styles.date}>{convertStringToDate(item.date)}</p>
-            <p className={styles.timeAndSeat}>
-              {item.time} {item.seat}
-            </p>
-          </div>
-          <div className={styles.icon}>
-            <ChevronRight />
-          </div>
+      <div
+        className={styles.reservationContainer}
+        key={reservation.id}
+        onClick={onClick}
+      >
+        <div className={styles.eachReservation}>
+          <p className={styles.date}>{convertStringToDate(reservation.date)}</p>
+          <p className={styles.timeAndSeat}>
+            {reservation.time} {reservation.seat}
+          </p>
         </div>
-      ))}
+        <div className={styles.icon}>
+          <ChevronRight />
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/MyReservation/index.tsx
+++ src/pages/MyReservation/index.tsx
@@ -7,49 +7,76 @@ import styles from './myReservation.module.scss';
 import { useAppDispatch, useAppSelector } from 'hooks/useRedux';
 import { MyReservation } from 'types/myReservation';
 
+import { openMyReservationModal } from 'reducers/modal';
+
 /** 더미 액션 작업 => 추후에 지워야함 */
 import { createAction } from '@reduxjs/toolkit';
 import reservations from './reservationDummy.json';
 
+import MyReservationModal from '../../components/MyReservationModal';
 import ReservationList from './ReservationList';
 
 function MyReservationPage() {
   const { myReservation } = useAppSelector(state => state.myReservation);
+  const { isMyRevervationModalOpen } = useAppSelector(state => state.modal);
   const [currentPage, setCurrentPage] = useState(1);
   const postsPerPage = 7;
-
-  const setMyReservation = createAction<MyReservation[]>(
-    'myReservation/setMyReservation',
-  );
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(setMyReservation(reservations));
-  }, []);
-
+  /* 페이지네이션 */
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentReservation = myReservation.slice(
     indexOfFirstPost,
     indexOfLastPost,
   );
-
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
+  const setMyReservation = createAction<MyReservation[]>(
+    'myReservation/setMyReservation',
+  );
+  const setMyReservationDetail = createAction<MyReservation>(
+    'myReservation/setMyReservationDetail',
+  );
+
+  useEffect(() => {
+    dispatch(setMyReservation(reservations));
+  }, []);
+
+  /* 클릭한 곳의 고유 id로 해당 reservation 데이터를 찾아서 
+  setMyReservationDetail을 통해 dispatch */
+  const handleOpenReservationModal = (id: number) => {
+    const reservation = currentReservation.find(item => item.id === id);
+    if (reservation) {
+      dispatch(setMyReservationDetail(reservation));
+    }
+    dispatch(openMyReservationModal());
+  };
+
   return (
-    <div className={styles.postContainer}>
-      <div className={styles.title}>내 예약 조회</div>
-      <div className={styles.lengthBox}>
-        <p>전체 {myReservation.length}개</p>
+    <>
+      {isMyRevervationModalOpen && <MyReservationModal />}
+      <div className={styles.postContainer}>
+        <div className={styles.title}>내 예약 조회</div>
+        <div className={styles.lengthBox}>
+          <p>전체 {myReservation.length}개</p>
+        </div>
+        {currentReservation.map((item, _) => (
+          <ReservationList
+            key={item.id}
+            reservation={item}
+            onClick={() => handleOpenReservationModal(item.id)}
+          />
+        ))}
+
+        <Pagination
+          postsPerPage={postsPerPage}
+          totalPosts={myReservation.length}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
       </div>
-      <ReservationList reservationList={currentReservation} />
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={myReservation.length}
-        paginate={paginate}
-        currentPage={currentPage}
-      />
-    </div>
+    </>
   );
 }
 
