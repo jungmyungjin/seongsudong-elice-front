e947b03ad551cc92d25560d4b3c4d8e30a31f8ac
[SW4/2반] 성치호 <clghdi9898@gmail.com>
2023-06-01 11:01:30 +0900

    feat: 게시물 상세페이지에서 수정버튼 클릭시 수정페이지로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/index.scss';
 import Board from './components/Board/Board';
 import PostsDetail from './components/Board/PostsDetail';
 import CreatePost from './components/Board/CreatePost';
+import PostUpdate from './components/Board/PostUpdate';
 import Reservation from 'components/Reservation';
 import Main from './pages/Main';
 import FloatingButton from 'components/common/FloatingButton';
@@ -23,6 +24,7 @@ function App() {
           <Route path='/post' element={<Board />} />
           <Route path='/post/:id' element={<PostsDetail />} />
           <Route path='/post/create' element={<CreatePost />} />
+          <Route path='/post/editPost/:id' element={<PostUpdate />} />
           <Route path='/reservation' element={<Reservation />} />
           <Route path='/mypage' element={<MyPage />} />
           <Route path='/mypage/mypost' element={<MyPost />} />
