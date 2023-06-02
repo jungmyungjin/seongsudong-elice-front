514ac245241aaaeab68164266e4d0f4e3b7cbb66
shinhayeong <judy.hayoung.shin@gmail.com>
2023-06-02 21:40:23 +0900

    remove : 사용 안하는 파일 삭제

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,14 +1,11 @@
 import { combineReducers } from 'redux';
 
-import userSlice from './user';
 import modalSlice from './modal';
 import menuSlice from './slideMenu';
 import myPostSlice from './myPost';
 import myReservationSlice from './myReservation';
 
 const rootReducer = combineReducers({
-  //슬라이스 리듀서를 한꺼번에 모아서 내보내기 위함
-  user: userSlice.reducer,
   modal: modalSlice.reducer,
   menu: menuSlice.reducer,
   myPost: myPostSlice.reducer,
