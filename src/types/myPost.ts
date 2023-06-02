9a75b7ffb00a212a811b3b7a9bd0c42f02dc3755
[SW4/2반] 성치호 <clghdi9898@gmail.com>
2023-06-02 16:14:16 +0900

    fix: api 테스트 중 코드 수정

--- src/types/myPost.ts
+++ src/types/myPost.ts
@@ -9,7 +9,7 @@ export interface myPost {
   id: number;
   category: string;
   title: string;
-  images: string;
+  images: string[];
   description: string;
   created_at: string;
   views: number;
