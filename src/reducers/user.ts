706eef469de018f7b583720916719e5d4a0d801c
jungmyungjin <myung2667@gmail.com>
2023-06-03 22:54:00 +0900

    feat : user 리듀서 추가
    유저의 로그인 유/무 정보와 사용자의 기본적인 정보가 담겨있는 리듀서 추가

---
+++ src/reducers/user.ts
@@ -0,0 +1,51 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+// 사용자 상태 타입 정의
+interface UserState {
+  loggedIn: boolean;
+  username: string | null;
+  course: string | null;
+  generation: number | string;
+}
+
+interface LogInPayload {
+  username: string;
+  course: string;
+  generation: number;
+}
+
+// 초기 사용자 상태
+const initialState: UserState = {
+  loggedIn: false,
+  username: null,
+  course: null,
+  generation: 0,
+};
+
+// 사용자 상태 slice 생성
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    // 로그인 액션
+    logIn: (state, action: PayloadAction<LogInPayload>) => {
+      state.loggedIn = true;
+      state.username = action.payload.username;
+      state.course = action.payload.course;
+      state.generation = action.payload.generation;
+    },
+    // 로그아웃 액션
+    logOut: state => {
+      state.loggedIn = false;
+      state.username = null;
+      state.course = null;
+      state.generation = 0;
+    },
+  },
+});
+
+// 액션 내보내기
+export const { logIn, logOut } = userSlice.actions;
+
+// 리듀서 내보내기
+export default userSlice;
