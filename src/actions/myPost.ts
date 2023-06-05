1e69ed67ce82e122deb8ed7b236e9ec17eed5646
shinhayeong <judy.hayoung.shin@gmail.com>
2023-06-05 19:43:42 +0900

    feat : 예약 조회 & 예약 취소 api 연결

--- src/actions/myPost.ts
+++ src/actions/myPost.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'http://localhost:3000';
 
 export const loadMyPost = createAsyncThunk('/post/loadMyPost', async () => {
   try {
--- src/pages/MyReservation/index.tsx
+++ src/pages/MyReservation/index.tsx
@@ -9,44 +9,41 @@ import { useAppDispatch, useAppSelector } from 'hooks/useRedux';
 import { MyReservation } from 'types/myReservation';
 
 import { openMyReservationModal } from 'reducers/modal';
+import { loadMyReservation } from 'actions/myReservation';
 
-/** 더미 액션 작업 => 추후에 지워야함 */
 import { createAction } from '@reduxjs/toolkit';
-import reservations from './reservationDummy.json';
 
 import MyReservationModal from '../../components/MyReservationModal';
 import ReservationList from './ReservationList';
 
 function MyReservationPage() {
-  const { myReservation } = useAppSelector(state => state.myReservation);
+  const { pastReservations, upcomingReservations } = useAppSelector(
+    state => state.myReservation,
+  );
+  const myReservationList = [...pastReservations, ...upcomingReservations];
+
   const { isMyRevervationModalOpen } = useAppSelector(state => state.modal);
   const postsPerPage = 10;
   const dispatch = useAppDispatch();
 
-  /* 페이지네이션 */
   const {
     currentItems: currentReservation,
     currentPage,
     goToPage,
-  } = usePaginate(myReservation, postsPerPage);
+  } = usePaginate(myReservationList, postsPerPage);
 
-  /** 더미 액션 작업 => 추후에 지워야함 */
-  const setMyReservation = createAction<MyReservation[]>(
-    'myReservation/setMyReservation',
-  );
-  /** 더미 액션 작업 => 추후에 지워야함 */
   const setMyReservationDetail = createAction<MyReservation>(
     'myReservation/setMyReservationDetail',
   );
 
   useEffect(() => {
-    dispatch(setMyReservation(reservations));
+    dispatch(loadMyReservation());
   }, []);
 
-  /* 클릭한 곳의 고유 id로 해당 reservation 데이터를 찾아서 
-  setMyReservationDetail을 통해 dispatch */
-  const handleOpenReservationModal = (id: number) => {
-    const reservation = currentReservation.find(item => item.id === id);
+  const handleOpenReservationModal = (id: string) => {
+    const reservation = currentReservation.find(
+      item => item.reservation_id === id,
+    );
     if (reservation) {
       dispatch(setMyReservationDetail(reservation));
     }
@@ -58,21 +55,25 @@ function MyReservationPage() {
       {isMyRevervationModalOpen && <MyReservationModal />}
       <div className={styles.postContainer}>
         <div className={styles.title}>내 예약 조회</div>
+        <div>
+          <button className={styles.upcomingReservations}>잡힌 예약</button>
+          <button className={styles.pastReservations}>지난 예약</button>
+        </div>
         <div className={styles.lengthBox}>
-          <p>전체 {myReservation.length}개</p>
+          <p>전체 {myReservationList.length}개</p>
         </div>
         <div className={styles.listBox}>
           {currentReservation.map((item, _) => (
             <ReservationList
-              key={item.id}
+              key={item.reservation_id}
               reservation={item}
-              onClick={() => handleOpenReservationModal(item.id)}
+              onClick={() => handleOpenReservationModal(item.reservation_id)}
             />
           ))}
         </div>
         <Pagination
           postsPerPage={postsPerPage}
-          totalPosts={myReservation.length}
+          totalPosts={myReservationList.length}
           paginate={goToPage}
           currentPage={currentPage}
         />
