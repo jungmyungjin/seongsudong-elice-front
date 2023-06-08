b478ac12a871b71ddc3892ddc3e3e84c249b35fd
shinhayeong <judy.hayoung.shin@gmail.com>
2023-06-09 02:11:54 +0900

    fix : 날짜 컨버팅

--- src/actions/myPost.ts
+++ src/actions/myPost.ts
@@ -6,8 +6,8 @@ export const loadMyPost = createAsyncThunk(
   async (userEmail: string | null) => {
     try {
       /** 로그인 성공 시 전역에 저장되어있는 이메일 여기다 넣음 */
-      const email = 'yunzoo0915@gmail.com';
-      // userEmail = email;
+      const email = 'email2@gmail.com';
+      userEmail = email;
       const response = await axios.get(
         `${process.env.REACT_APP_BACKEND_ADDRESS}/posts?email=${userEmail}`,
       );
