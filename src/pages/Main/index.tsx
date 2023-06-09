3c35677bdcf92c83c385c30d2c4c3f7272eed616
jungmyungjin <myung2667@gmail.com>
2023-06-09 16:50:44 +0900

    feat: 메인페이지 게시판 반영, 게시판 버튼 공용 컴포넌트로 수정

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './main.module.scss';
 import MainImage from 'components/MainImage';
-import Boards from './Boards';
 import MainReservationSection from './MainReservationSection';
-import volum from 'assets/volum.svg';
-import Chat from 'assets/Chat.svg';
+import axios from 'axios';
+
+import CustomLink from 'components/common/Link';
+import PostList from 'components/common/PostList';
 
 const Main = (): React.ReactElement => {
+  const [freeBoard, setFreeboard] = useState([]);
+  const [noticeBoard, setNoticeboard] = useState([]);
+
+  const fetchPosts = async () => {
+    const freeBoardPromise = await axios.get(
+      `${process.env.REACT_APP_BACKEND_ADDRESS}/posts/top?category=자유게시판`,
+    );
+    const noticeBoardPromise = await axios.get(
+      `${process.env.REACT_APP_BACKEND_ADDRESS}/posts/recent?category=공지게시판`,
+    );
+
+    try {
+      const [freeBoardResponse, noticeBoardResponse] = await Promise.all([
+        freeBoardPromise,
+        noticeBoardPromise,
+      ]);
+      const freeBoardData = freeBoardResponse.data;
+      const noticeBoardData = noticeBoardResponse.data;
+
+      setNoticeboard(noticeBoardData);
+      setFreeboard(freeBoardData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  fetchPosts();
+
   return (
     <div className={styles.MainLayout}>
       <section className={styles.imageSlider}>
@@ -14,7 +42,16 @@ const Main = (): React.ReactElement => {
       </section>
 
       <section className={styles.noticeBoard}>
-        <Boards icon={volum} boardName='공지사항' route='/post' />
+        <div className={styles.boardButton}>
+          <CustomLink
+            key={'notice'}
+            to={'/post'}
+            title={'공지사항'}
+            icon={'notice'}
+            right={true}
+          />
+        </div>
+        <PostList posts={noticeBoard} />
       </section>
 
       <section className={styles.reservation}>
@@ -22,7 +59,16 @@ const Main = (): React.ReactElement => {
       </section>
 
       <section className={styles.freeBoard}>
-        <Boards icon={Chat} boardName='자유 게시판' route='/post/free' />
+        <div className={styles.boardButton}>
+          <CustomLink
+            key={'notice'}
+            to={'/post/free'}
+            title={'자유 게시판'}
+            icon={'chat'}
+            right={true}
+          />
+        </div>
+        <PostList posts={freeBoard} />
       </section>
     </div>
   );
