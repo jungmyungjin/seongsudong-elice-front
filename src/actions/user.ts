7a568644cd0496278b08c564fd89fed105e7ba64
shinhayeong <judy.hayoung.shin@gmail.com>
2023-06-13 21:54:55 +0900

    feat : 로그아웃 & 탈퇴 완료

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -14,15 +14,19 @@ export const logout = createAsyncThunk('/user/logout', async () => {
   }
 });
 
-export const deleteUser = createAsyncThunk('/user/delete', async () => {
-  try {
-    const response = await axios.post(
-      `${process.env.REACT_APP_BACKEND_ADDRESS}/members/delete`,
-      { withCredentials: true },
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-});
+export const deleteUser = createAsyncThunk(
+  '/user/delete',
+  async (email: string | null) => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_BACKEND_ADDRESS}/members/delete`,
+        { email: email },
+        { withCredentials: true },
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+);
