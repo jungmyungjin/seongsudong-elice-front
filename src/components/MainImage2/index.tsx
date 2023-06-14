683c751b65f176e3dd7d317b193299140cb3f60b
jungmyungjin <myung2667@gmail.com>
2023-06-14 21:45:29 +0900

    feat : 메인페이지 버전2

---
+++ src/components/MainImage2/index.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import { getDate } from 'utils/getTime';
+import styles from './mainImage.module.scss';
+import CountUp from 'react-countup';
+import axios from 'axios';
+
+const MainImage = (): React.ReactElement => {
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+  const [remainSeat, setRemainSeat] = useState(0);
+
+  const now = getDate();
+  const api =
+    process.env.REACT_APP_BACKEND_ADDRESS +
+      '/reservations/seat-check?reservation_date=' +
+      now || '';
+
+  const getRemainSeat = async () => {
+    try {
+      const response = await axios.get(api, {
+        withCredentials: true,
+      });
+      if (response.status === 200) {
+        setRemainSeat(Object.keys(response.data).length - 2); // -2 : 미팅룸
+      }
+    } catch (error) {
+      console.log('Error getRemainSeat :', error);
+    }
+  };
+
+  useEffect(() => {
+    getRemainSeat();
+  }, []);
+
+  return (
+    <div className={styles.mainImage}>
+      <video
+        className={isVideoLoaded ? styles.mainVideo : styles.mainVideoHide}
+        src='/videos/PulpleSmoke.mp4'
+        autoPlay
+        loop
+        muted
+        playsInline
+        disablePictureInPicture
+        controlsList='nodownload'
+        onLoadedData={() => setIsVideoLoaded(true)}
+      />
+      <div className={styles.titleLayout}>
+        <p className={(styles.title, styles.typing)}>성수동 엘리스</p>
+      </div>
+
+      <div className={styles.todayInfo}>
+        <div className={styles.todayVisitor}>
+          <div className={styles.infoTitle}>오늘 이용자 수</div>
+          <div className={styles.infoValue}>
+            <span>
+              <CountUp delay={5} end={195} />
+            </span>
+            <span>명</span>
+          </div>
+        </div>
+
+        <div className={styles.todaySeat}>
+          <div className={styles.infoTitle}>잔여 좌석 수</div>
+          <div className={styles.infoValue}>
+            <span>
+              <CountUp
+                key={remainSeat}
+                delay={4.5}
+                duration={4.75}
+                end={remainSeat}
+              />
+            </span>
+            <span>명</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainImage;
