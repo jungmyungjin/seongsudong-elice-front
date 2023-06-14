d1c88edd42fbe526f8bbf9571a3da118a9e34ea1
shinhayeong <judy.hayoung.shin@gmail.com>
2023-06-14 18:57:17 +0900

    feat : 중복되는 날짜 한 번만 렌더링

--- src/utils/convertDate.ts
+++ src/utils/convertDate.ts
@@ -1,5 +1,5 @@
 /**
- * @returns {string} "2023년 5월 31일 (수요일) 오전 12:26" 형식으로 현재 시간 내보내줌
+ * @returns {string} "2023년 5월 31일 (수요일) 오전 12:26" 형식으로 "현재 시간" 내보내줌
  */
 export function convertDate(now?: Date): string {
   const today = new Date();
@@ -20,6 +20,45 @@ export function convertDate(now?: Date): string {
   const formattedDate = `${year}년 ${month}월 ${day}일 (${dayOfWeek}) ${amPm} ${formattedHour}:${formattedMinute}`;
 
   return formattedDate;
+}
+
+/**
+ *
+ * @param dateTimeString  string으로 들어오는 날짜
+ * @returns 2023년 6월 14일 (수요일) 오후 05:00'
+ */
+export function getCustomDateString(dateTimeString: string) {
+  const date = new Date(dateTimeString);
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const dayOfWeek = ['일', '월', '화', '수', '목', '금', '토'][date.getDay()];
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  const formattedDate = `${year}년 ${month}월 ${day}일 (${dayOfWeek}요일)`;
+  const formattedHours = hours > 12 ? `오후 ${hours - 12}` : `오전 ${hours}`;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formattedTime = `${formattedHours}:${formattedMinutes}`;
+
+  return `${formattedDate} ${formattedTime}`;
+}
+
+/**
+ *
+ * @param dateTimeString  string으로 들어오는 날짜
+ * @returns 요일까지만 출력됨요 > 월, 일, 요일에 따라서 요일이 다르면 한 번만 출력하게 하기 위해서 만든 함수
+ */
+export function getPrevDateString(dateString: string): string {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}-${month < 10 ? '0' + month : month}-${
+    day < 10 ? '0' + day : day
+  }`;
 }
 
 /**
