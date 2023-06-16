19e9e3bbed47a591b026b2bd4b86739707e0041f
shinhayeong <judy.hayoung.shin@gmail.com>
2023-06-16 19:47:44 +0900

    fix : logout api 메서드 변경

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -3,7 +3,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const logout = createAsyncThunk('/user/logout', async () => {
   try {
-    const response = await axios.post(
+    const response = await axios.delete(
       `${process.env.REACT_APP_BACKEND_ADDRESS}/members/logout`,
       { withCredentials: true },
     );
